07337ccb2e488d62ab73e4f7ba0af211db934116
hossain <hossainahamedk@gmail.com>
2024-05-11 01:42:20 +0600

    completed initial setup

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css';
+import Root from './routes/Root.jsx';
+import ErrorPage from './pages/ErrorPage.jsx';
+import HomePage from './pages/HomePage.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <HomePage></HomePage>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1 className="text-5xl text-center">Home Page</h1>
+        </div>
+    );
+};
+
+export default HomePage;
