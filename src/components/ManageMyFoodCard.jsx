655fb0013861b8c54774a4f64c54a179961f66a4
hossain <hossainahamedk@gmail.com>
2024-05-15 00:08:48 +0600

    fix delete function

--- src/components/ManageMyFoodCard.jsx
+++ src/components/ManageMyFoodCard.jsx
@@ -8,33 +8,35 @@ const ManageMyFoodCard = ({ myFood, control, setControl }) => {
         donator_email, food_status } = myFood;
 
     const handleDelete = (id) => {
-        fetch(`https://save-food-server.vercel.app/delete-food/${id}`, {
-            method: "DELETE",
-        })
-            .then(res => res.json())
-            .then(data => {
-                if (data.deletedCount > 0) {
-                    Swal.fire({
-                        title: "Are you sure?",
-                        text: "You won't be able to revert this!",
-                        icon: "warning",
-                        showCancelButton: true,
-                        confirmButtonColor: "#3085d6",
-                        cancelButtonColor: "#d33",
-                        confirmButtonText: "Yes, delete it!"
-                    }).then((result) => {
-                        if (result.isConfirmed) {
-                            Swal.fire({
-                                title: "Deleted!",
-                                text: "Your file has been deleted.",
-                                icon: "success"
-                            });
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`https://save-food-server.vercel.app/delete-food/${id}`, {
+                    method: "DELETE",
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
                             setControl(!control);
                         }
-                    });
-                }
-            })
+                    })
+                Swal.fire({
+                    title: "Deleted!",
+                    text: "Your file has been deleted.",
+                    icon: "success"
+                });
+            }
+        });
     }
+
     return (
         <tr>
             <td>
